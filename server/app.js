8f9a6dedb52a9ba9c482acf99c6f8dd53586efda
Juri Aristov <juri.aristov@gmail.com>
2024-05-24 10:07:45 +0300

    Friday start

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const removeHeaders = require('./middleware/removeHeaders');
-// const indexRouter = require('./routes/index.router');
+const removeHeaders = require('./middlewares/removeHeaders');
+const indexRouter = require('./routes/index.routes');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(removeHeaders);
 app.use(express.json());
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT} port`);
